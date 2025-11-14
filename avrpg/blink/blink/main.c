854ce0fc8421f4f7e209fbb0eb35667490f29bce
nitheeshapjak-beep <nitheeshapjak@gmail.com>
2025-11-15 00:34:45 +0530

    Change blink delay to 3 seconds
    Updated the delay duration for the blinking LED to 3 seconds.

--- avrpg/blink/blink/main.c
+++ avrpg/blink/blink/main.c
@@ -11,13 +11,13 @@
 
 int main(void)
 {
-  DDRB=0b00100000;
+  DDRB=0b00100000;// dedicating 5 TH PIN OF PORT B AS OUTPUT
     while (1) 
     {
-		PORTB=0b00100000;
-		_delay_ms(1000);
-		PORTB=0b00000000;
-		_delay_ms(1000);
+		PORTB=0b00100000; //MAKE OUTPUT HIGH AT 5TH PIN OF PORT B
+		_delay_ms(3000);//MAKE DELAY OF 3 SEC
+		PORTB=0b00000000;//MAKE OUTPUT LOW AT 5TH PIN OF PORT B
+		_delay_ms(3000);//MAKE DELAY OF 3 SEC
 		
     }
 }
